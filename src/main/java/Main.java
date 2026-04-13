473ae6edc58cfb6dab39a41b5738eef2e5747c86
leonardolscuccimarra <leonardolscuccimarra@gmail.com>
2026-04-13 12:25:27 -0300

    Initial Commit/13-04/Version 0.1 print a consola con comprobación de credencial de usuario (opcional)

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        CUI objCUI = new CUI();
+        objCUI.printLogo();
+        UserLogin objUserLogin = new UserLogin(); // Se pueden cargar argumento username y password para hacer validaciones
+    }
+}
---
+++ src/main/java/UserLogin.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class UserLogin {
+
+    public UserLogin(){
+        scannerLogIn();
+        System.out.println("Bienvenido");
+    }
+    public UserLogin(String username, String password){
+        validateLogIn(username, password, scannerLogIn());
+    }
+    private String[] scannerLogIn(){
+        Scanner sc = new Scanner(System.in);
+        String[] inputs = new String[2];
+        System.out.println("Introduzca su usuario:");
+        inputs[0] = sc.nextLine();
+        System.out.println("Introduzca su contraseña:");
+        inputs[1] = sc.nextLine();
+
+        return inputs;
+    }
+    private void validateLogIn(String username,String password, String[] inputs){
+        if (inputs[0].equals(username) && inputs[1].equals(password)){
+            System.out.println("Bienvenido "+ username);
+        } else {
+            System.out.println("Credenciales incorrectas, intente nuevamente"+ System.lineSeparator());
+            validateLogIn(username,password,inputs);
+        }
+    }
+}
